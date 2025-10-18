116ca0832b783cfceb29f32f0527d4dbbed5194d
Shpetim-Logistiqo <shpetim@logistiqo.com>
2025-10-18 17:10:44 +0200

    Added repositories

---
+++ Backend/BeLoyal/src/main/java/com/shabanaj/beloyal/Repository/BusinessProfileRepository.java
@@ -0,0 +1,7 @@
+package com.shabanaj.beloyal.Repository;
+
+import com.shabanaj.beloyal.Entity.BusinessProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BusinessProfileRepository extends JpaRepository<BusinessProfile, Long> {
+}
---
+++ Backend/BeLoyal/src/main/java/com/shabanaj/beloyal/Repository/CustomerProfileRepository.java
@@ -0,0 +1,7 @@
+package com.shabanaj.beloyal.Repository;
+
+import com.shabanaj.beloyal.Entity.CustomerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerProfileRepository extends JpaRepository<CustomerProfile, Long>{
+}
